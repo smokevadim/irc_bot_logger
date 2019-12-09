d94de85e62bcb0d82d6330b67db77f31fef09c71
Vadim Gazizov <nosmokingvadim@gmail.com>
2019-12-09 09:00:47 +0200

    in work

--- vars.py
+++ vars.py
@@ -1,5 +1,14 @@
-SERVER_NAME = 'irc.freenode.net'
-NICKNAME = 'rustik'
-MINIMUM_USERS = 10
-ATTEMPS_TO_RECONNECT = 5
-STEP_CHANNELS = 40
+SERVER_NAME = 'irc.freenode.net'# IRC server
+NICKNAME = 'smokevadim'		# nickname 
+USER_NAME = 'smokevadim'	# registered username to IRC server
+PASSWORD = '12345'		# password to IRC server
+MINIMUM_USERS = 500		# connect to channels with minimum users
+ATTEMPS_TO_RECONNECT = 5 	# attemps to try reconnect on one bot
+STEP_CHANNELS = 20 		# number of one time channels subcribe (server not alowed more than 20)
+
+### for email notification
+### if use gmail.com need to enable this option on https://myaccount.google.com/lesssecureapps
+ADDR_FROM = 'smoke.kaliningrad@gmail.com' # email sender
+ADDR_TO = '' # email receiver
+EMAIL_PASSWORD = '' # password to email server
+EMAIL_SERVER = 'smtp.gmail.com'  # smtp server
